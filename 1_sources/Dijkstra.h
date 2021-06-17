955e8861fb09ab112bf5c6567d1c910c9ad80a47
Dianka Levchenko <dianka_levchenko@inbox.ru>
2021-06-17 21:01:29 +0300

    Updated dijkstra algorithm.

--- 1_sources/Dijkstra.h
+++ 1_sources/Dijkstra.h
@@ -2,101 +2,155 @@
 #ifndef DIJKSTRA_H
 #define DIJKSTRA_H
 
-typedef struct nodes {
-    int dest;
-    int cost;
-}node;
 
-class Graph {
-    int n;
-    list<node>* adjList;
+
+class Dijkstra {
+
 private:
-    void showList(int src, list<node> lt) {
-        list<node> ::iterator i;
-        node tempNode;
 
-        for (i = lt.begin(); i != lt.end(); i++) {
-            tempNode = *i;
-            cout << "(" << src << ")---(" << tempNode.dest << "|" << tempNode.cost << ") ";
-        }
-        cout << endl;
-    }
-public:
-    Graph() {
-        n = 0;
-    }
+    static const int MAX_COST = 1000;
 
-    Graph(int nodeCount) {
-        n = nodeCount;
-        adjList = new list<node>[n];
-    }
-
-    void addEdge(int source, int dest, int cost) {
-        node newNode;
-        newNode.dest = dest;
-        newNode.cost = cost;
-        adjList[source].push_back(newNode);
-    }
-
-    void displayEdges() {
-        for (int i = 0; i < n; i++) {
-            list<node> tempList = adjList[i];
-            showList(i, tempList);
+    static void init_costs(int** costs, int** graph, int countOfVertex) {
+        for (int i = 0; i < countOfVertex; i++) {
+            for (int j = 0; j < countOfVertex; j++) {
+                if (graph[i][j] == 0) {
+                    costs[i][j] = MAX_COST;
+                }
+                else {
+                    costs[i][j] = graph[i][j];
+                }
+            }
         }
     }
-    friend class Dijkstra;
-};
-
-
-class Dijkstra
-{
-private:
     
+    static void calc_distances(int** cost, int* distance, int* pred, int countOfVertex, int startVertex) {
+        int* visited = new int[countOfVertex];
+        int calculatedVertex = 0;
+        int minDistance = MAX_COST;
+        int nextNode = 0;
+
+
+        for (int i = 0; i < countOfVertex; i++) {
+            distance[i] = cost[startVertex][i];
+            pred[i] = startVertex;
+            visited[i] = 0;
+        }
+
+        distance[startVertex] = 0;
+        visited[startVertex] = 1;
+        calculatedVertex = 1;
+
+        while (calculatedVertex < countOfVertex - 1) {
+            minDistance = MAX_COST;
+            for (int i = 0; i < countOfVertex; i++) {
+                if (distance[i] < minDistance && !visited[i]) {
+                    minDistance = distance[i];
+                    nextNode = i;
+                }
+            }
+
+            visited[nextNode] = 1;
+
+
+            for (int i = 0; i < countOfVertex; i++) {
+                if (!visited[i])
+                    if (minDistance + cost[nextNode][i] < distance[i]) {
+                        distance[i] = minDistance + cost[nextNode][i];
+                        pred[i] = nextNode;
+                    }
+
+            }
+
+            calculatedVertex++;
+        }
+    }
+
+    static void print_distances(int* distance, int* pred, int countOfVertex, int startVertex) {
+        for (int i = 0; i < countOfVertex; i++) {
+            if (i != startVertex) {
+                if (distance[i] == MAX_COST) {
+                    cout << "No way from vertex #" << startVertex << " to vertex #" << i;
+                }
+                else {
+                    cout << "Distance from vertex #" << startVertex << " to vertex #" << i
+                        << " is equal to " << distance[i]
+                        << ". Path: " << i;
+
+                    int j = i;
+
+                    do {
+                        j = pred[j];
+                        cout << " <= " << j;
+                    } while (j != startVertex);
+                }
+                cout << endl;
+            }
+        }
+    }
+
+
+    static void dijkstra(int** graph, int startVertex, int countOfVertex) {
+        int* distance = new int[countOfVertex];
+        int* pred = new int[countOfVertex];
+        int** costs = new int* [countOfVertex];
+
+        for (int i = 0; i < countOfVertex; i++) {
+            costs[i] = new int[countOfVertex];
+        }
+
+
+        init_costs(costs, graph, countOfVertex);
+        calc_distances(costs, distance, pred, countOfVertex, startVertex);
+        print_distances(distance, pred, countOfVertex, startVertex);
+    }
 
 public:
-    //Graph g, int* dist, int* prev, int start
-    static void run(Graph g, int* dist, int* prev, int start) {
-        int n = g.n;
-        
-        
-        for (int u = 0; u < n; u++) {
-            dist[u] = 9999;   //set as infinity
-            prev[u] = -1;    //undefined previous
-        }
-        dist[start] = 0;   //distance of start is 0
-        set<int> S;       //create empty set S
-        list<int> Q;
-        for (int u = 0; u < n; u++) {
-            Q.push_back(u);    //add each node into queue
-        }
-        while (!Q.empty()) {
-            list<int> ::iterator i;
-            i = min_element(Q.begin(), Q.end());
-            int u = *i; //the minimum element from queue
-            Q.remove(u);
-            S.insert(u); //add u in the set
-            list<node> ::iterator it;
-            for (it = g.adjList[u].begin(); it != g.adjList[u].end(); it++) {
-                if ((dist[u] + (it->cost)) < dist[it->dest]) { //relax (u,v)
-                    dist[it->dest] = (dist[u] + (it->cost));
-                    prev[it->dest] = u;
-                }
-            }
-        }
-        //g.showList(2, *g.adjList);
-        for (int i = 0; i < n; i++)
-            if (i != start) {
-                if (dist[i] == 9999) {
-                    cout << start << " to " << i << ", way impassable." << endl;
-                    continue;
-                }
-                cout << start << " to " << i << ", cost: " << dist[i] << ", previous: " << prev[i] << endl;
-            }
-                
-    };
-    friend class Graph;
-};
 
+    static void run() {
+        int countOfVertex = 5;
+        int startVertex = 2;
+
+        int** weights = new int* [countOfVertex] {
+            new int[countOfVertex] { 0, 10, 3, 0, 0 },
+                new int[countOfVertex] { 0, 0, 1, 2, 0 },
+                new int[countOfVertex] { 0, 4, 0, 8, 2 },
+                new int[countOfVertex] { 0, 0, 0, 0, 7 },
+                new int[countOfVertex] { 0, 0, 0, 9, 0 }
+        };
+
+
+        dijkstra(weights, startVertex, countOfVertex);
+    }
+
+
+    static void run_with_input() {
+        cout << "Please, enter count of vertex: ";
+        int countOfVertex = 0;
+
+        cin >> countOfVertex;
+
+        int** weights = new int* [countOfVertex];
+        for (int i = 0; i < countOfVertex; i++) {
+            weights[i] = new int[countOfVertex];
+        }
+
+        for (int i = 0; i < countOfVertex; i++) {
+            for (int j = 0; j < countOfVertex; j++) {
+                cout << "weights[" << i << ", " << j << "] = ";
+                cin >> weights[i][j];
+            }
+        }
+
+        cout << "Please, enter start vertex: ";
+        int startVertex = 0;
+
+        cin >> startVertex;
+
+
+        dijkstra(weights, startVertex, countOfVertex);
+    }
+
+};
 
 
 #endif
--- 1_sources/GraphsAlgorithms.cpp
+++ 1_sources/GraphsAlgorithms.cpp
@@ -66,24 +66,7 @@ int main()
 			utils::resize_window(700, 700);
 			utils::clearScreen();
 
-
-			int const n = 5;
-			Graph G(n);
-			int dist[n], prev[n];
-			int start = 2;
-
-
-			G.addEdge(0, 1, 10);
-			G.addEdge(0, 2, 3);
-			G.addEdge(1, 2, 1);
-			G.addEdge(1, 3, 2);
-			G.addEdge(2, 1, 4);
-			G.addEdge(2, 3, 8);
-			G.addEdge(2, 4, 2);
-			G.addEdge(3, 4, 7);
-			G.addEdge(4, 3, 9);
-
-			Dijkstra::run(G, dist, prev, start);
+			Dijkstra::run();
 
 			getchar();
 			break;
